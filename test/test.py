910b61ef8a858215fa768685fdab776a228ebd82
Gaurav Chaurasia <gaurav.181cv155@nitk.edu.in>
2022-03-16 22:56:29 +0530

    convert user input to df

--- test/test.py
+++ test/test.py
@@ -1,11 +1,12 @@
 import requests
 
 input_data={
-    'Q': 1,
+    'P': 1,
     'Tmax': 2,
     'Tmin': 3
 }
+# 2014-12-30	0.062500	4.756	-10.230	8771.143378
 
-resp = requests.post(url="http://localhost:5000/predict?q=1&tmin=2&tmax=3")
+resp = requests.post(url="http://localhost:5000/predict?p=0.062500&tmin=-10.230&tmax=4.756")
 
 print(resp.text)
